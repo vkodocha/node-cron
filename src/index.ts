101f4dd12dcac96069bafdc4f1978bcea821e470
V <v@vmac.ch>
2023-05-29 13:21:50 +0200

    Adding a second task

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ import cron from 'node-cron';
 cron.schedule(`*/1 * * * *`, async () => {
   console.log(`running your task...`);
 });
+
+cron.schedule(`0/30 0 0 ? * * *`, async () => {
+  console.log(`a second task`);
+});
